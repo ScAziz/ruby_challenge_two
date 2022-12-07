0efda9120c715d9a537b48c9771f09375d5ff5e6
steveaziz <stephenaziz.sa@gmail.com>
2022-12-07 13:42:17 +0000

    ready to be reviewed

--- lib/password_manager.rb
+++ lib/password_manager.rb
@@ -1,3 +1,27 @@
 
 class PasswordManager
+    def initialize
+        @passwords = {}
+    end
+
+    def valid?(password)
+        return password.length >= 8 && password[/[!|@|$|%|&]/] ? true : false
+    end 
+
+    def add(service, password)
+        if valid?(password)
+            @passwords[service] = password
+        else 
+            return "Invalid password"
+        end
+    end
+
+    def password_for(service)
+        return @passwords[service]
+    end
+
+    def services
+        return @passwords.keys
+    end
 end
+
